8ac53a9eb1bda29f34c808f4a0555352fc75deda
adbindal <adbindal@microsoft.com>
2020-07-18 23:24:46 +0530

    Implementing Backend build and deploy code to the the publish script
    - TODO: remove hardcoded mapping from Function Project to Function Resource and arrive at some intelligent way to handle that
    - TODO: add client build and deploy code inside the script
    - TODO: modularize the scripts to become callable as functions from the appRegistration script

--- deployment/Publish.ps1
+++ deployment/Publish.ps1
@@ -1,12 +1,33 @@
+function Write-Log {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Message
+    ) 
+    $now = (Get-Date).ToString();
+    Write-Host "[$now] - $Message";
+}
+
+function Write-Title {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Title
+    )
+    Write-Host ''
+    Write-Host ''
+    Write-Host '============================================================='
+    Write-Host $Title
+    Write-Host '============================================================='
+    Write-Host ''
+    Write-Host ''    
+}
+
 
 # Will be used for accessing specific resources based on the naming convension used in the ARM template deployment
 $resourceGroupName = Read-Host -Prompt 'Please provide the Resource Group name.'
 
 Push-Location "../backend"
 
-Write-Host '======================';
-Write-Host 'Installing the backend';
-Write-Host '======================';
+Write-Title "Installing the backend"
 
 # Go over all the [.csproj] files under the [Functions] folder and for each of them do the following:
 # 1. Restore
@@ -17,62 +38,62 @@ Write-Host '======================';
 # 
 # Best way would be to create a separate script for a single function deployment that receives parameters
 
-Write-Host 'Restoring'
-dotnet restore
+$AZ_FUNCTIONS = @{
+    "Edna.AssignmentLearnContent"="edna-lti-ab-learn";
+    "Edna.AssignmentLinks"="edna-lti-ab-links";
+    "Edna.Assignments"="edna-lti-ab-assignment";
+    "Edna.Connect"="edna-lti-ab-connect";
+    "Edna.Platforms"="edna-lti-ab-platfomrs";
+    "Edna.Users"="edna-lti-ab-users"
+}
 
-Write-Host 'Building'
-dotnet build --configuration RELEASE
+$fnRegex = "Functions/**/*.csproj"
+$functions = Get-ChildItem -Path $fnRegex -Recurse
+$publishRoot = 'Artifacts'
+foreach ($function in $functions) {
+    $fnName = $function.Directory.Name
+    $projectDir = $function.Directory
 
-Write-Host 'Publishing'
-dotnet publish --configuration RELEASE --output Publish
+    $publishDir = Join-Path $publishRoot $fnName
+    Write-Log "Building -- $publishDir"
+    dotnet publish $projectDir --configuration RELEASE --output $publishDir --nologo --verbosity quiet
 
-Write-Host '==============================';
-Write-Host 'Backend Installation Completed';
-Write-Host '==============================';
+    $zipPath = Join-Path $publishRoot "$fnName.zip"
+    Write-Log "Zipping Artifacts -- $zipPath"
+    Compress-Archive -Path "$publishDir/*" -DestinationPath $zipPath
+
+    $azFunction = $AZ_FUNCTIONS[$fnName]
+    Write-Log "Deploying to Azure -- $azFunction"
+    az functionapp deployment config-zip -g $resourceGroupName -n $azFunction --src $zipPath
+}
+
+Write-Log 'Deleting Artifacts'
+Remove-Item -LiteralPath $publishRoot -Recurse -Force
+
+Write-Title 'Backend Installation Completed'
+
+Write-Title 'Installing the client'
 
 Push-Location "../client"
 
-
-Write-Host '=====================';
-Write-Host 'Installing the client';
-Write-Host '=====================';
-Write-Host '';
-Write-Host 'Running npm install';
-Write-Host '-------------------';
+Write-Log 'Running npm install';
 npm i
-Write-Host '';
-Write-Host '';
-Write-Host '';
 
 # TODO
 # Fetch the needed values from different Azure resources and rewrite the [.env.production] file.
 
-Write-Host 'Building React';
-Write-Host '--------------';
-npm run-script build
-Write-Host '';
+Write-Log 'Building React'
+# npm run build
 
-Write-Host 'Deploying as a static WebApp';
-Write-Host '----------------------------';
-Write-Host '';
-Write-Host 'Delete existing website content (Just in case of a redeploy)';
-az storage blob delete-batch --account-name psednaclienttests --source '$web'
+Write-Log 'Deploying as a static WebApp';
+
+Write-Log 'Delete existing website content (Just in case of a redeploy)';
+# az storage blob delete-batch --account-name psednaclienttests --source '$web'
 
 Write-Host 'Uploading build content to the static website storage container';
-az storage blob upload-batch -s 'build' -d '$web' --account-name psednaclienttests
+# az storage blob upload-batch -s 'build' -d '$web' --account-name psednaclienttests
 
-Write-Host '';
-
-# TODO:
-# Add the static website URL to the app registration redirect URL
-
-Write-Host '=============================';
-Write-Host 'Client Installation Completed';
-Write-Host '=============================';
-
-Write-Host '';
-Write-Host '';
-Write-Host '';
+Write-Title 'Client Installation Completed'
 
 # Check if running Powershell ISE
 if ($psISE)
@@ -82,5 +103,5 @@ if ($psISE)
 else
 {
     Write-Host -NoNewLine 'Press any key to continue...';
-    $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 }
