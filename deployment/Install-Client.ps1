2d13ec15573bcb4ff6fc8f1de26ca38ebe8e3654
adbindal <adbindal@microsoft.com>
2020-07-22 03:39:20 +0530

    Code Cleanup (4/n): Minor refactoring in client installation workflow and updating publish script to invoke it correctly

--- deployment/Install-Client.ps1
+++ deployment/Install-Client.ps1
@@ -10,6 +10,28 @@ enum DotEnv {
     REACT_APP_EDNA_PLATFORM_SERVICE_URL
 }
 
+function Get-ADAppScope ([string]$AppId) {
+    Write-Log "Getting Default scope for App -- $AppId"
+    return "api://$AppId/user_impersonation"
+}
+
+function Get-TenantId ([string] $AppId) {
+    # Assumes that the user is currently signed into correct subscription
+    Write-Log "Getting Tenant Id for App -- $AppId"
+    #$app = az ad sp show --id "$AppId" | ConvertFrom-Json
+    $account = az account show | ConvertFrom-Json
+    # if (!$app) {
+    #     throw 'Unable to get App details for AD App Id: $AppId'
+    # }
+    return $account.tenantId
+    #return $app.appOwnerTenantId
+}
+
+function Get-ServiceUrl ([string]$ServiceName)  {
+    Write-Log "Getting Service Url for Function App -- $ServiceName"
+    return "https://$ServiceName.azurewebsites.net/api"
+}
+
 function Export-DotEnv ([hashtable]$config, [string]$fileName) {
     # TODO: Test the config for completeness of values inside fileName
     Write-Log "Updating $fileName with new config"
@@ -50,33 +72,12 @@ function Update-ClientConfig {
         [string]$StaticWebsiteUrl
     )
 
-    function Get-ADAppScope  {
-        Write-Log "Getting Default scope for App -- $AppId"
-        return "api://$AppId/user_impersonation"
-    }
-
-    function Get-TenantId {
-        # Assumes that the user is currently signed into correct subscription
-        Write-Log "Getting Tenant Id for App -- $AppId"
-        #$app = az ad sp show --id "$AppId" | ConvertFrom-Json
-        $account = az account show | ConvertFrom-Json
-        # if (!$app) {
-        #     throw 'Unable to get App details for AD App Id: $AppId'
-        # }
-        return $account.tenantId
-        #return $app.appOwnerTenantId
-    }
-
-    function Get-ServiceUrl ([string]$ServiceName)  {
-        Write-Log "Getting Service Url for Function App -- $ServiceName"
-        return "https://$ServiceName.azurewebsites.net/api"
-    }
 
     $Config = @{
         [DotEnv]::REACT_APP_EDNA_AAD_CLIENT_ID="$AppId";
         [DotEnv]::REACT_APP_EDNA_MAIN_URL="$StaticWebsiteUrl";
-        [DotEnv]::REACT_APP_EDNA_DEFAULT_SCOPE="$(Get-ADAppScope)";
-        [DotEnv]::REACT_APP_EDNA_TENANT_ID="$(Get-TenantId)";
+        [DotEnv]::REACT_APP_EDNA_DEFAULT_SCOPE="$(Get-ADAppScope $AppId)";
+        [DotEnv]::REACT_APP_EDNA_TENANT_ID="$(Get-TenantId $AppId)";
         [DotEnv]::REACT_APP_EDNA_LEARN_CONTENT="$(Get-ServiceUrl $LearnContentFunctionAppName)";
         [DotEnv]::REACT_APP_EDNA_LINKS_SERVICE_URL="$(Get-ServiceUrl $LinksFunctionAppName)";
         [DotEnv]::REACT_APP_EDNA_ASSIGNMENT_SERVICE_URL="$(Get-ServiceUrl $AssignmentsFunctionAppName)";
@@ -91,28 +92,29 @@ function Install-Client {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [string]$ClientRoot,
+        [string]$SourceRoot,
         [Parameter(Mandatory)]
-        [string]$StaticWebsite
+        [string]$StaticWebsiteStorageAccount
     )
     
-    Push-Location $ClientRoot
+    Push-Location $SourceRoot
+
+    try {        
+        Write-Log 'Running npm install'
+        npm ci
+    
+        Write-Log 'Building Client App'
+        npm run build
         
-    Write-Log 'Running npm install';
-    npm ci
-
-    Write-Log 'Building React'
-    npm run build
-    
-    Write-Log "Deploying as a static WebApp -- $StaticWebsite";
-    
-    $web = '$web'
-    $clientStorageAccount = $StaticWebsite
-    Write-Log 'Delete existing website content (Just in case of a redeploy)';
-    az storage blob delete-batch --account-name $clientStorageAccount --source $web
-    
-    Write-Log 'Uploading build content to the static website storage container';
-    az storage blob upload-batch -s 'build' -d $web --account-name $clientStorageAccount 
-
-    Pop-Location
+        Write-Log "Deploying as a static WebApp -- $StaticWebsiteStorageAccount"
+        
+        Write-Log 'Delete existing website content (Just in case of a redeploy)'
+        az storage blob delete-batch --account-name $StaticWebsiteStorageAccount --source '$web'
+        
+        Write-Log 'Uploading build content to the static website storage container'
+        az storage blob upload-batch -s 'build' -d '$web' --account-name $StaticWebsiteStorageAccount 
+    }
+    finally {
+        Pop-Location
+    }
 }
