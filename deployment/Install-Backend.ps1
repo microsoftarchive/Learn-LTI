0c0c74065861b7cf05a3f5d7eb17c72cb661ff1b
adbindal <adbindal@microsoft.com>
2020-07-22 02:30:56 +0530

    Code Cleanup (2/n): Updating backend code to be dumb about the function app names and start expecting all the function apps as arguments
    - Temporarily commenting out Client installation code as it will not work with newer scaffolding

--- deployment/Install-Backend.ps1
+++ deployment/Install-Backend.ps1
@@ -1,63 +1,112 @@
-function Publish-FunctionApp {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string]$fnName,
-        [Parameter(Mandatory)]
-        [string]$projectDir,
-        [Parameter(Mandatory)]
-        [string]$outputDir,
-        [Parameter(Mandatory)]
-        [string]$resourceGroupName,
-        [Parameter(Mandatory)]
-        [string]$functionAppName
-    )
-
-    $publishDir = Join-Path $outputDir $fnName
-    Write-Log "Building -- $publishDir"
-    dotnet publish $projectDir --configuration RELEASE --output $publishDir --nologo --verbosity quiet
-
-    $zipPath = Join-Path $outputDir "$fnName.zip"
-    Write-Log "Zipping Artifacts -- $zipPath"
-    Compress-Archive -Path "$publishDir/*" -DestinationPath $zipPath
-
-    Write-Log "Deploying to Azure -- $resourceGroupName/$functionAppName"
-    $result = az functionapp deployment source config-zip -g $resourceGroupName -n $functionAppName --src $zipPath | ConvertFrom-Json
-    if(!$result) {
-        throw "Failed to deploy $fnName to $functionAppName"
-    }
+# List of known Azure Functions to Install / Publish.
+# Update this list on adding / updating any Function. 
+enum VALID_FUNCTIONS {
+    AssignmentLearnContent;
+    AssignmentLinks;
+    Assignments;
+    Connect;
+    Platforms;
+    Users
 }
-function Install-Backend {
- 
-    [CmdletBinding()]
+
+function Get-Project([VALID_FUNCTIONS]$FunctionName) {
+    $PathRegex = "Functions/**/Edna.$FunctionName.csproj"
+    Get-ChildItem -Path $PathRegex -Recurse
+}
+
+function Publish-FunctionApp {
+
+    [CmdletBinding(SupportsShouldProcess)]
     param (
         [Parameter(Mandatory)]
-        [string]$BackendRoot,
+        [string]$FunctionName,
+        [Parameter(Mandatory)]
+        [string]$ProjectDir,
+        [Parameter(Mandatory)]
+        [string]$OutputDir,
         [Parameter(Mandatory)]
         [string]$ResourceGroupName,
         [Parameter(Mandatory)]
-        [string[]]$FunctionAppNames
+        [string]$FunctionAppName
     )
 
-    Push-Location $BackendRoot
-    
-    $publishRoot = 'Artifacts'
-    if(Test-Path $publishRoot) {
-        Write-Log "Deleting old Artifacts"
-        Remove-Item -LiteralPath $publishRoot -Recurse -Force
+    $PublishDir = Join-Path $OutputDir $FunctionName
+    Write-Log "Building -- $PublishDir"
+    dotnet publish $ProjectDir --configuration RELEASE --output $PublishDir --nologo --verbosity quiet
+
+    $ArchivePath = Join-Path $OutputDir "$FunctionName.zip"
+    Write-Log "Zipping Artifacts -- $ArchivePath"
+    Compress-Archive -Path "$PublishDir/*" -DestinationPath $ArchivePath
+
+    Write-Log "Deploying to Azure -- $ResourceGroupName/$FunctionAppName"
+    $result = az functionapp deployment source config-zip -g $ResourceGroupName -n $FunctionAppName --src $ArchivePath | ConvertFrom-Json
+    if(!$result) {
+        throw "Failed to deploy $FunctionName to $FunctionAppName"
     }
-    
-    $fnRegex = "Functions/**/*.csproj"
-    $functions = Get-ChildItem -Path $fnRegex -Recurse
-    foreach ($function in $functions) {
-        $fnName = $VALID_FUNCTIONS | Where-Object { $function.Directory.Name -like "Edna.*$_" }
-        $projectDir = $function.Directory
-        $functionAppName = $FunctionAppNames | Where-Object { $_ -like "$fnName-*" }
-        Publish-FunctionApp $fnName $projectDir $publishRoot $ResourceGroupName $functionAppName
+}
+
+function Install-Backend {
+ 
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [string]$SourceRoot,
+        [Parameter(Mandatory)]
+        [string]$ResourceGroupName,
+        [string]$LearnContentFunctionAppName,
+        [string]$LinksFunctionAppName,
+        [string]$AssignmentsFunctionAppName,
+        [string]$ConnectFunctionAppName,
+        [string]$PlatformsFunctionAppName,
+        [string]$UsersFunctionAppName
+    )
+
+    Push-Location $SourceRoot
+
+    try {
+        $PublishRoot = 'Artifacts'
+        if(Test-Path $PublishRoot) {
+            Write-Log "Deleting old Artifacts"
+            Remove-Item -LiteralPath $PublishRoot -Recurse -Force
+        }
+        
+        try {            
+            $Functions = [System.Enum]::GetNames([VALID_FUNCTIONS])
+            foreach ($Function in $Functions) {
+
+                Write-Log "Installing Function -- $Function"
+                $Project = Get-Project $Function
+
+                $PublishParams = @{
+                    FunctionName = $Function;
+                    ProjectDir = $($Project.Directory);
+                    OutputDir = $PublishRoot;
+                    ResourceGroupName = $ResourceGroupName
+                }
+
+                $FunctionAppName = & {
+                    switch ($Function) {
+                        "AssignmentLearnContent"    { return $LearnContentFunctionAppName }
+                        "AssignmentLinks"           { return $LinksFunctionAppName }
+                        "Assignments"               { return $AssignmentsFunctionAppName }
+                        "Connect"                   { return $ConnectFunctionAppName }
+                        "Platforms"                 { return $PlatformsFunctionAppName }
+                        "Users"                     { return $UsersFunctionAppName }
+                    }
+                }
+                if ($FunctionAppName) { 
+                    $PublishParams.FunctionAppName = $FunctionAppName
+                }
+
+                Publish-FunctionApp @PublishParams
+            }
+        }
+        finally {            
+            Write-Log 'Deleting Artifacts'
+            Remove-Item -LiteralPath $publishRoot -Recurse -Force
+        }
     }
-    
-    Write-Log 'Deleting Artifacts'
-    Remove-Item -LiteralPath $publishRoot -Recurse -Force
-    
-    Pop-Location
+    finally {
+        Pop-Location   
+    }    
 }
