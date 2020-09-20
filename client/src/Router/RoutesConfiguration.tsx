e198ae8a93b1c744b24c9dca89bdd217a74e43a1
maleekha <maleekha@microsoft.com>
2020-09-20 23:37:59 +0530

    restored routeConfigs file

--- client/src/Router/RoutesConfiguration.tsx
+++ client/src/Router/RoutesConfiguration.tsx
@@ -20,7 +20,7 @@ export const configurationRoutes: RouteProps[] = [
   {
     name: pagesDisplayNames.MSLEARN,
     url: routesConstants.MSLEARN,
-    icon: 'D365TalentLearn',
+    icon: 'LearningTools',
     component: MicrosoftLearnPage
   }
 ];
