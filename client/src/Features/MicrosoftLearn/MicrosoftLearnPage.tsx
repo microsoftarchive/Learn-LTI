d1c1a9abbef71a005ff463f002ad5a03d7c1550f
maleekha <maleekha@microsoft.com>
2020-09-23 19:38:53 +0530

    removed UI changes, moving them to a separate branch

--- client/src/Features/MicrosoftLearn/MicrosoftLearnPage.tsx
+++ client/src/Features/MicrosoftLearn/MicrosoftLearnPage.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useObserver } from 'mobx-react-lite';
 import { useStore } from '../../Stores/Core';
 import { MicrosoftLearnList } from './MicrosoftLearnList';
-// import { PageWrapper } from '../../Core/Components/Common/PageWrapper';
+import { PageWrapper } from '../../Core/Components/Common/PageWrapper';
 import { themedClassNames } from '../../Core/Utils/FluentUI';
 import { SimpleComponentStyles, IStylesOnly, IThemeOnlyProps } from '../../Core/Utils/FluentUI/typings.fluent-ui';
 import { styled, Separator, mergeStyles } from '@fluentui/react';
@@ -12,68 +12,30 @@ import {
   MicrosoftLearnSelectedItemsListStyles
 } from './MicrosoftLearnSelectedItemsList';
 import { getCommonSpacingStyle } from './MicrosoftLearnStyles';
-import { MicrosoftLearnFilterPane } from './MicrosoftLearnFilterPane';
-import { debounce } from 'lodash';
-import { MicrosoftLearnFilterTags } from './MicrosoftLearnFilterTags';
+import { pagesDisplayNames } from '../../Router/Consts';
 
-
-type MicrosoftLearnPageStyles = SimpleComponentStyles<'root' | 'separator' | 'wrapper'>;
+type MicrosoftLearnPageStyles = SimpleComponentStyles<'root' | 'separator'>;
 
 const MicrosoftLearnPageInner = ({ styles }: IStylesOnly<MicrosoftLearnPageStyles>): JSX.Element => {
   const learnStore = useStore('microsoftLearnStore');
-  // const learnFilterStore = useStore('microsoftLearnFilterStore');
-
   useEffect(() => {
     if (learnStore.catalog == null) {
-      learnStore.initializeCatalog()
+      learnStore.initializeCatalog();
     }
   }, [learnStore]);
 
   const classes = themedClassNames(styles);
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-      const debouncedHandleResize = debounce(function handleResize() {
-          setWidth(window.innerWidth)
-      }, 500)
-      window.addEventListener('resize', debouncedHandleResize)
-      return () => {
-          window.removeEventListener('resize', debouncedHandleResize)        
-      }
-  })
-
-
   return useObserver(() => {
-
-    if(width>768){
-      return (
-        <div className={classes.wrapper}>
-          <MicrosoftLearnFilterPane /> 
-          <div className={classes.root}>
+    return (
+      <PageWrapper title={pagesDisplayNames.MSLEARN}>
+        <div className={classes.root}>
           <MicrosoftLearnSearch styles={themedClassNames(microsoftLearnSearchStyles)} />
           <MicrosoftLearnSelectedItemsList styles={themedClassNames(microsoftLearnSelectedItemsStyles)} />
           <Separator className={classes.separator} />
-          <MicrosoftLearnFilterTags />
           <MicrosoftLearnList />
         </div>
-        </div>
-      )
-    }
-
-    else{
-      return(
-        <div className={classes.wrapper}>
-        <div className={classes.root}>
-        <MicrosoftLearnSearch styles={themedClassNames(microsoftLearnSearchStyles)} />
-        <MicrosoftLearnSelectedItemsList styles={themedClassNames(microsoftLearnSelectedItemsStyles)} />
-        <Separator className={classes.separator} />
-        <MicrosoftLearnFilterPane />         
-        <MicrosoftLearnFilterTags />
-        <MicrosoftLearnList />
-      </div>
-      </div>
-      )
-    }
+      </PageWrapper>
+    );
   });
 };
 
@@ -90,20 +52,13 @@ const microsoftLearnPageStyles = ({ theme }: IThemeOnlyProps): MicrosoftLearnPag
       fontSize: 0,
       padding: 0,
       marginTop: theme.spacing.s1,
-      marginBottom: `calc(${theme.spacing.l1}*0.5 + ${theme.spacing.s1})`,
+      marginBottom: `calc(${theme.spacing.l1} + ${theme.spacing.s1})`,
       selectors: {
         '::before': {
           backgroundColor: theme.semanticColors.bodyDivider
         }
       }
     })
-  ],
-  wrapper: [
-    {
-      display:'flex', 
-      flexDirection: 'row',
-      marginTop: theme.spacing.l1
-    }
   ]
 });
 
@@ -116,5 +71,3 @@ const microsoftLearnSelectedItemsStyles = ({
   header: [getCommonSpacingStyle(theme)]
 });
 export const MicrosoftLearnPage = styled(MicrosoftLearnPageInner, microsoftLearnPageStyles);
-
-
