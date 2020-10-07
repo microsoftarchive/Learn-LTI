a50cfbf9b7101673f11fae494b84e0540338c7e4
nehalilani <nelilani@microsoft.com>
2020-10-07 16:02:59 +0530

    resolving conflict with main branch

--- backend/Utils/Edna.Utils.Http/Edna.Utils.Http/HttpHeadersExtensions.cs
+++ backend/Utils/Edna.Utils.Http/Edna.Utils.Http/HttpHeadersExtensions.cs
@@ -1,13 +1,10 @@
-﻿// --------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-// --------------------------------------------------------------------------------------------
-
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Edna.Utils.Http
 {
