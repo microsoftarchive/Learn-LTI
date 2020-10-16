12efe2437ddcbea1c9a77c95825bfac48e120d85
maleekha <maleekha@microsoft.com>
2020-10-16 18:30:04 +0530

    Adding license header to new files

--- client/src/Core/Components/AssignmentUpdateFailureMessageBar.tsx
+++ client/src/Core/Components/AssignmentUpdateFailureMessageBar.tsx
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ *--------------------------------------------------------------------------------------------*/
+
 import { AnimationClassNames, IMessageBarStyles, MessageBar, MessageBarType, styled } from '@fluentui/react';
 import { useObserver } from 'mobx-react-lite';
 import React from 'react';
