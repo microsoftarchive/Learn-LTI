db93ba9a417889a40928c9c8dfe2366c0c636e5b
Lee Stott <leestott@microsoft.com>
2021-06-10 13:51:15 +0100

    Scripts and Batch files
    updated cleanup script

--- deployment/cleanup.bat
+++ deployment/cleanup.bat
@@ -5,5 +5,5 @@ REM Licensed under the MIT license.
 REM --------------------------------------------------------------------------------------------
 
 REM Triggers Learn-LTI Cleanup process for bad installations 
-REM This script will remove all orphaned resources from your azure subscription.
-PowerShell.exe -ExecutionPolicy Bypass -File .\Cleanup.ps1
+REM This script will remove all orphaned resources from your Azure Subscription which were associated to a failed deployment of the Microsoft Learn LTI Application.
+PowerShell.exe -ExecutionPolicy Bypass -File .\cleanup.ps1
