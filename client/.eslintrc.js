513ac93239fa609ea4a4dbd369c9c84d19fbdedf
nehalilani <neha.lilani@gmail.com>
2022-04-06 10:36:18 +0530

    Update .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
     }
   },
   extends: [
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended'
