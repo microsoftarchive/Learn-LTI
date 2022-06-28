4f703f8de6764e8174d74aa92be09eb0f2524381
DuyNguyen19993010 <yuddayama@gmail.com>
2022-06-28 17:45:25 +0100

    Deploy configurations

--- deployment/Deploy.ps1
+++ deployment/Deploy.ps1
@@ -5,8 +5,8 @@
 
 [CmdletBinding()]
 param (
-    [string]$ResourceGroupName = "RB_cloudT2_MSLearnLTI",
-    [string]$AppName = "RB_cloudT2_MS-Learn-Lti-Tool-App",
+    [string]$ResourceGroupName = "DN_cloudT2_MSLearnLTI",
+    [string]$AppName = "DN_cloudT2_MS-Learn-Lti-Tool-App",
     [switch]$UseActiveAzureAccount,
     [string]$SubscriptionNameOrId = $null,
     [string]$LocationName = $null
