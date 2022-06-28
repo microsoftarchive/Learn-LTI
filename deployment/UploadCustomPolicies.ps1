4cce285ae0ee15e9260beeac613dc73985710af7
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2022-06-28 11:42:03 +0100

    Debugged UploadCustomPolicies.ps1 regarding it not being able to find the path to the CustomPolicies; and errors caused by an incorrectly formatted TRUSTFRAMEWORKBASE.xml template

--- deployment/UploadCustomPolicies.ps1
+++ deployment/UploadCustomPolicies.ps1
@@ -12,18 +12,16 @@ function Write-Title([string]$Title) {
 #getting the values we want to replace from the templates
 Write-Title "STEP 1: Getting values to use in the custom policies?"
 $B2cTenantName = Read-Host "What is the name of your b2c tenant?"
-$B2cTenantObjectId = Read-Host "What is the object ID of your b2c tenant?"
 $ProxyIdentityExperienceFrameworkAppId = Read-Host "What is the application ID of the ProxyIdentityExperienceFramework application you created?"
 $IdentityExperienceFrameworkAppId = Read-Host "What is the application ID of the IdentityExperienceFramework application you created?"
 $FacebookId = Read-Host "What is the application ID of the Facebook application you created?"
-$FacebookSecret = Read-Host "What is the value of your facebook applications secret?"
 
 
 # looping through each CustomPolicyTemplate and creating bases of them in CustomTemplates
 Write-Title "STEP 2: Creating template custom policies"
 Get-ChildItem ".\CustomPolicyTemplates\" | 
 Foreach-Object {
-    ((Get-Content -path $_.FullName -Raw)) | Set-Content -path ("./CustomPolicy/"+$_.Name)
+    ((Get-Content -path $_.FullName -Raw)) | Set-Content -path (".\CustomPolicy\"+$_.Name)
 }
 
 
@@ -32,17 +30,13 @@ Foreach-Object {
 Write-Title "STEP 3: Replacing values in template custom policies to generate finalised custom policies"
 Get-ChildItem ".\CustomPolicy\" | 
 Foreach-Object {
-    ((Get-Content -path $_.FullName -Raw) -replace '<<B2CTenantName>>', $B2cTenantName) |  Set-Content -path ("./CustomPolicy/"+$_.Name)
+    ((Get-Content -path $_.FullName -Raw) -replace '<<B2CTenantName>>', $B2cTenantName) |  Set-Content -path (".\CustomPolicy\"+$_.Name)
 
-    ((Get-Content -path $_.FullName -Raw) -replace '<<B2CTenantObjectID>>', $B2cTenantObjectId) |  Set-Content -path ("./CustomPolicy/"+$_.Name)
-
-    ((Get-Content -path $_.FullName -Raw) -replace '<<ProxyIdentityExperienceFrameworkAppId>>', $ProxyIdentityExperienceFrameworkAppId) |  Set-Content -path ("./CustomPolicy/"+$_.Name)
+    ((Get-Content -path $_.FullName -Raw) -replace '<<ProxyIdentityExperienceFrameworkAppId>>', $ProxyIdentityExperienceFrameworkAppId) |  Set-Content -path (".\CustomPolicy\"+$_.Name)
     
-    ((Get-Content -path $_.FullName -Raw) -replace '<<IdentityExperienceFrameworkAppId>>', $IdentityExperienceFrameworkAppId) |  Set-Content -path ("./CustomPolicy/"+$_.Name)
+    ((Get-Content -path $_.FullName -Raw) -replace '<<IdentityExperienceFrameworkAppId>>', $IdentityExperienceFrameworkAppId) |  Set-Content -path (".\CustomPolicy\"+$_.Name)
     
-    ((Get-Content -path $_.FullName -Raw) -replace '<<FacebookId>>', $FacebookId) |  Set-Content -path ("./CustomPolicy/"+$_.Name)
-    
-    ((Get-Content -path $_.FullName -Raw) -replace '<<FacebookSecret>>', $FacebookSecret) |  Set-Content -path ("./CustomPolicy/"+$_.Name)
+    ((Get-Content -path $_.FullName -Raw) -replace '<<FacebookId>>', $FacebookId) |  Set-Content -path (".\CustomPolicy\"+$_.Name)
 
 }
 
@@ -51,12 +45,29 @@ Foreach-Object {
 Write-Title "STEP 4: Uploading the custom policies to the b2c tenant"
 
 $B2cTenantDomain = Read-Host "What is your B2C tenants domain (e.g. mytenant.onmicrosoft.com)?"
+Write-Host "Importing Module AzureADPreview"
 Import-Module AzureADPreview
+Write-Host "Connecting to the tenant"
 Connect-AzureAD -Tenant $B2cTenantDomain
+
 # Order matters in the uploads - do not modify the order
-New-AzureADMSTrustFrameworkPolicy -InputFilePath "./CustomPolicy/TRUSTFRAMEWORKBASE.xml"
-New-AzureADMSTrustFrameworkPolicy -InputFilePath "./CustomPolicy/TRUSTFRAMEWORKLOCALIZATION.xml"
-New-AzureADMSTrustFrameworkPolicy -InputFilePath "./CustomPolicy/TRUSTFRAMEWORKEXTENSIONS.xml"
-New-AzureADMSTrustFrameworkPolicy -InputFilePath "./CustomPolicy/SIGNUP_SIGNIN.xml"
-New-AzureADMSTrustFrameworkPolicy -InputFilePath "./CustomPolicy/PROFILEEDIT.xml"
-New-AzureADMSTrustFrameworkPolicy -InputFilePath "./CustomPolicy/PASSWORDRESET.xml"
+$currentDir = Get-Location
+$currentDir = $currentDir.tostring()
+
+Write-Host "Uploading custom policy TRUSTFRAMEWORKBASE"
+New-AzureADMSTrustFrameworkPolicy -InputFilePath ($currentDir+"\CustomPolicy\TRUSTFRAMEWORKBASE.xml")
+
+Write-Host "Uploading custom policy TRUSTFRAMEWORKLOCALIZATION"
+New-AzureADMSTrustFrameworkPolicy -InputFilePath ($currentDir+"\CustomPolicy\TRUSTFRAMEWORKLOCALIZATION.xml")
+
+Write-Host "Uploading custom policy TRUSTFRAMEWORKEXTENSIONS"
+New-AzureADMSTrustFrameworkPolicy -InputFilePath ($currentDir+"\CustomPolicy\TRUSTFRAMEWORKEXTENSIONS.xml")
+
+Write-Host "Uploading custom policy SIGNUP_SIGNIN"
+New-AzureADMSTrustFrameworkPolicy -InputFilePath ($currentDir+"\CustomPolicy\SIGNUP_SIGNIN.xml")
+
+Write-Host "Uploading custom policy PROFILEEDIT"
+New-AzureADMSTrustFrameworkPolicy -InputFilePath ($currentDir+"\CustomPolicy\PROFILEEDIT.xml")
+
+Write-Host "Uploading custom policy PASSWORDRESET"
+New-AzureADMSTrustFrameworkPolicy -InputFilePath ($currentDir+"\CustomPolicy\PASSWORDRESET.xml")
