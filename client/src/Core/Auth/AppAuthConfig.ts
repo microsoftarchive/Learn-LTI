202c9b0e3db6d9356d6961989125d610ff82c05b
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2022-07-28 15:44:20 +0100

    Fixed merge error

--- client/src/Core/Auth/AppAuthConfig.ts
+++ client/src/Core/Auth/AppAuthConfig.ts
@@ -74,29 +74,15 @@ const config: Configuration = {
   }
 };
 
-<<<<<<< HEAD
 const authParams: AuthenticationParameters = {
   //TODO - remove the hard coded ltimoodleb2c!!!
   //old hard coded scopes
-  scopes: ['https://' + process.env.REACT_APP_EDNA_B2C_TENANT! + '.onmicrosoft.com/api/b2c.read'] // RB: 'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read' // b2c scope
-  // scopes: [ //AD scopes
-  //   //'https://ltimoodleb2c.onmicrosoft.com/api/user_impersonation',
-  //   'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read'
-  // ] // RB: 'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read'
+  // scopes: ['https://' + process.env.REACT_APP_EDNA_B2C_TENANT! + '.onmicrosoft.com/api/b2c.read'] // RB: 'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read' // b2c scope
+  scopes: [ //AD scopes
+    //'https://ltimoodleb2c.onmicrosoft.com/api/user_impersonation',
+    'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read'
+  ] // RB: 'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read'
 };
-=======
-// Todo, may no longer be neccessary
-// const authParams: AuthenticationParameters = {
-//   //scopes: ['https://' + process.env.REACT_APP_EDNA_B2C_TENANT! + '.onmicrosoft.com/api/b2c.read'] // RB: 'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read'
-//   scopes: [
-//     // TODO: unclear what is needed for custom policies
-//     //'https://ltimoodleb2c.onmicrosoft.com/api/user_impersonation',
-//     'https://ltimoodleb2c.onmicrosoft.com/api/b2c.read',
-//     'openid',
-//     'profile'
-//   ]
-// };
->>>>>>> origin/all-test1
 
 // Todo, may no longer be neccessary
 // const options: IMsalAuthProviderConfig = {
